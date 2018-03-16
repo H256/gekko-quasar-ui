53010b5a1a7a8bf0d8d5828c22cea6bd94d0c765
H256 <hansi256@web.de>
2018-03-16 22:49:28 +0100

    use 'hash' mode for vue routing to fix 404 error pages

--- quasar.conf.js
+++ quasar.conf.js
@@ -24,7 +24,7 @@ module.exports = function (ctx) {
     },
     build: {
       scopeHoisting: true,
-      vueRouterMode: 'history',
+      vueRouterMode: 'hash',//'history',
       // gzip: true,
       // analyze: true,
       // extractCSS: false,
