4c56bd0c7f22e537987ec037965542c501507c44
H256 <hansi256@web.de>
2018-03-17 14:28:09 +0100

    new compiled dist

--- src/components/gekko/singleWatcher.vue
+++ src/components/gekko/singleWatcher.vue
@@ -4,56 +4,59 @@
       <h2 class="text-negative">ERROR: Unknown watcher</h2>
       <p>Gekko does'nt know what watcher this is.</p>
     </div>
-    <h3>Market Watcher</h3>
+    <template v-if="data">
+      <h3>Market Watcher</h3>
       <div class="row">
         <div class="col-2">
-           <strong>Market:</strong>
+          <strong>Market:</strong>
         </div>
         <div class="col-10">{{data.watch.exchange}}</div>
       </div>
       <div class="row">
         <div class="col-2">
-           <strong>Currency</strong>
+          <strong>Currency</strong>
         </div>
         <div class="col-10">{{data.watch.currency}}</div>
       </div>
       <div class="row">
         <div class="col-2">
-           <strong>Asset:</strong>
+          <strong>Asset:</strong>
         </div>
         <div class="col-10">{{data.watch.asset}}</div>
       </div>
-     <h3>Statistics</h3>
-     <div class="row" v-if="isLoading">
-       <div class="col-2 text-center">
+      <h3>Statistics</h3>
+      <div class="row" v-if="isLoading">
+        <div class="col-2 text-center">
           <q-spinner-bars size="36" color="tertiary" />
-       </div>
-     </div>
-     <div class="row" v-if="!isLoading && data.firstCandle">
+        </div>
+      </div>
+      <div class="row" v-if="!isLoading && data.firstCandle">
         <div class="col-2">
-           <strong>Watching since:</strong>
+          <strong>Watching since:</strong>
         </div>
         <div class="col-10">{{fmt(data.firstCandle.start)}}</div>
-     </div>
-     <div class="row" v-if="!isLoading && data.lastCandle">
+      </div>
+      <div class="row" v-if="!isLoading && data.lastCandle">
         <div class="col-2">
-           <strong>Received data until:</strong>
+          <strong>Received data until:</strong>
         </div>
         <div class="col-10">{{fmt(data.lastCandle.start)}}</div>
-     </div>
-     <div class="row" v-if="!isLoading && data.firstCandle">
+      </div>
+      <div class="row" v-if="!isLoading && data.firstCandle">
         <div class="col-2">
-           <strong>Data spanning:</strong>
+          <strong>Data spanning:</strong>
         </div>
         <div class="col-10">{{dataSpanning}}</div>
-     </div>
+      </div>
 
-     <div class="row" v-if="!isLoading && candleFetch === 'fetching'">
-       <div class="col-2 text-center">
+      <div class="row" v-if="!isLoading && candleFetch === 'fetching'">
+        <div class="col-2 text-center">
           <q-spinner-bars size="36" color="tertiary" />
-       </div>
-     </div>
-     <echart v-if="candles.length" :candles="chartData.candles" :trades="chartData.trades" height="500" />
+        </div>
+      </div>
+      <echart v-if="candles.length" :candles="chartData.candles" :trades="chartData.trades" height="500" />
+    </template>
+
   </q-page>
 </template>
 
@@ -67,7 +70,9 @@ import echart from '../global/chart'
 
 export default {
   created: function() {
-    if (!this.isLoading) this.getCandles();
+    if (!this.isLoading) {
+      this.getCandles();
+    }
   },
   components: {
     echart
@@ -103,7 +108,7 @@ export default {
     }
   },
   watch: {
-    "data.lastCandle.start": function() {
+    'data.lastCandle.start': function() {
       this.candleFetch = "dirty";
     },
     data: function(val, prev) {
@@ -111,7 +116,7 @@ export default {
 
       if (!complete) return;
 
-      if (this.candleFetch !== "fetched") this.getCandles();
+      if (this.candleFetch !== 'fetched') this.getCandles();
     }
   },
   methods: {
@@ -127,30 +132,20 @@ export default {
       // max 7 days of data
       let from = Math.max(
         moment.utc(this.data.firstCandle.start).unix(),
-        moment
-          .utc(to)
-          .subtract(7, "days")
-          .unix()
+        moment.utc(to).subtract(7, "days").unix()
       );
 
       // TODO...
       const diff = to - from;
       let candleSize = 60;
-      if (diff < 60 * 60 * 24)
-        if (diff < 60 * 60 * 12)
-          // a day
-          // 3 hours
+      if (diff < 60 * 60 * 24)// a day
+        if (diff < 60 * 60 * 12)// 3 hours
           candleSize = 1;
-        else candleSize = 5;
+        else
+          candleSize = 5;
 
-      from = moment
-        .unix(from)
-        .utc()
-        .format();
-      to = moment
-        .unix(to)
-        .utc()
-        .format();
+      from = moment.unix(from).utc().format();
+      to = moment.unix(to).utc().format();
 
       let config = {
         watch: this.data.watch,
@@ -165,17 +160,15 @@ export default {
       this.$axios
         .post(this.$store.state.config.apiBaseUrl + "getCandles", config)
         .then(response => {
+          //console.log("Successfully loaded candles", response.data)
           this.candleFetch = "fetched";
           this.candles = response.data.map(c => {
-            c.start = moment
-              .unix(c.start)
-              .utc()
-              .format();
+            c.start = moment.unix(c.start).utc().format();
             return c;
           });
         })
         .catch(error => {
-          console.log(error);
+          console.log("Error on getting candle Data", error);
           /*this.$q.notify({
             type: "negative",
             message: "Error getting candle-data."
