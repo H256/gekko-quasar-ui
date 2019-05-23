cfbfcf1d42283c30b0f11806b87eff0d80554568
Klemens Wittig <hansi256@web.de>
2019-05-23 18:06:14 +0200

    - remove linebreak on dates   since this breaks date-recognition in moment.js...   preparation for pr #21 - cleanup code

--- src/store/backtest/actions.js
+++ src/store/backtest/actions.js
@@ -1,5 +1,6 @@
 import _ from 'lodash'
 import moment from 'moment'
+
 /**
  * set the retrieved backtest result
  * @param state
@@ -11,28 +12,25 @@ export const setBacktestResult = (state, result) => {
     // transform to correct js dates
     if(result){
       if(_.has(result,'stratCandles') && result.stratCandles.length > 0){
-        let rC = result.stratCandles.map((item)=>{
-          item.start = moment.unix(item.start).format('YYYY-MM-DD[\n]HH:mm'); //new Date(item.start);
+        result.stratCandles = result.stratCandles.map((item) => {
+          item.start = moment.unix(item.start).format('YYYY-MM-DD HH:mm'); //new Date(item.start);
           return item;
         });
-        result.stratCandles = rC;
       }
       if(_.has(result,'stratUpdates') && result.stratUpdates.length > 0){
-        let rC = result.stratUpdates.map((item)=>{
-          item.date = moment.unix(item.date).format('YYYY-MM-DD[\n]HH:mm');
+        result.stratUpdates = result.stratUpdates.map((item) => {
+          item.date = moment.unix(item.date).format('YYYY-MM-DD HH:mm');
           return item;
         });
-        result.stratUpdates = rC;
       }
       if(_.has(result,'trades') && result.trades.length > 0){
-        let rC = result.trades.map((item)=>{
-          item.date = moment.unix(item.date).format('YYYY-MM-DD[\n]HH:mm');
+        result.trades = result.trades.map((item) => {
+          item.date = moment.unix(item.date).format('YYYY-MM-DD HH:mm');
           return item;
         });
-        result.trades = rC;
       }
     }
     state.commit('setResult', result);
     resolve();
   });
-}
+};
